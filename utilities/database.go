afd4329d29d1c18538aa76b831fa2836a13450ed
Fambegbe Olamileke <olamileke.f@gmail.com>
2025-03-19 11:25:08 +0100

    feat: added validation checks for signup

--- utilities/database.go
+++ utilities/database.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	githubmysql "github.com/go-sql-driver/mysql"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -16,9 +19,9 @@ func GetDatabaseObject() *gorm.DB {
 		return database
 	}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT"), os.Getenv("DATABASE_NAME"))
+	location, _ := time.LoadLocation("Europe/London")
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.New(mysql.Config{DSNConfig: &githubmysql.Config{User: os.Getenv("DATABASE_USERNAME"), Passwd: os.Getenv("DATABASE_PASSWORD"), DBName: os.Getenv("DATABASE_NAME"), Net: "tcp", Addr: fmt.Sprintf("%s:%s", os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_PORT")), ParseTime: true, Loc: location}}), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("There was a problem connecting to the database")
--- validators/auth.go
+++ validators/auth.go
@@ -3,9 +3,15 @@ package validators
 import (
 	"carbon/controllers"
 	"carbon/dto"
+	"carbon/models"
+	"carbon/utilities"
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
 func SignupValidator(context echo.Context) error {
@@ -17,6 +23,18 @@ func SignupValidator(context echo.Context) error {
 
 	if err := context.Validate(signupDto); err != nil {
 		return err
+	}
+
+	database := utilities.GetDatabaseObject()
+
+	var user models.User
+
+	signupDto.Email = strings.ToLower(signupDto.Email)
+
+	result := database.Where("email = ?", signupDto.Email).First(&user)
+
+	if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return utilities.ThrowException(context, &utilities.Exception{StatusCode: http.StatusBadRequest, Error: "AUTH_001", Message: fmt.Sprintf("User with email address %s exists already", signupDto.Email)})
 	}
 
 	context.Set("signupDto", signupDto)
